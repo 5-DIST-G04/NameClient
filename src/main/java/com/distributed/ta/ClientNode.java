84d97b2e12a78bd3b9e623769eec3d7a04ed2c5c
Yves De Boeck <yves.deboeck@student.uantwerpen.be>
2018-12-12 14:00:31 +0100

    update

--- src/main/java/com/distributed/ta/ClientNode.java
+++ src/main/java/com/distributed/ta/ClientNode.java
@@ -1,18 +1,18 @@
 package com.distributed.ta;
 
 
-import com.distributed.common.MulticastPublisher;
-import com.distributed.common.MulticastReceiver;
-import com.distributed.common.ServerComm;
+import com.distributed.common.*;
+import com.distributed.common.File;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-import com.distributed.common.Node;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Inet4Address;
 import java.net.URI;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ClientNode {
@@ -34,6 +34,7 @@ public class ClientNode {
         StartGrizzlyContainer();
         StartMulticastReceiver();
         SendInitMulticast();
+        VerifyLocalFiles();
 
     }
 
@@ -73,6 +74,61 @@ public class ClientNode {
     private void stopServer(){
         server.stop();
     }
+
+
+    private void VerifyLocalFiles () {
+        File aDirectory = new File("src/main/java/com/distributed/data");
+        String[] filesInDir = aDirectory.list();
+
+
+        for ( int i=0; i<filesInDir.length; i++ )
+        {
+
+            replicateFile( filesInDir[i]);
+        }
+    }
+
+    private void replicateFile(String s) {
+        //vraag aan server voor de node waar de HAshNodeID <= hashFile
+        File file = new File(s);
+        file.getFileLog().put(nodeData.getThisNode(),0);        //local=0, replicated=1, downloaded=2
+        nodeData.addLocal(file);
+        int fileHash = file.getHash();
+        Node replicationNode = ServerComm.getReplicationNode(fileHash);
+        if (replicationNode==nodeData.getThisNode()) {
+            file.setFileOwner(false);
+            ClientComm.replicate(file, nodeData.getPrevNode());     //dit klopt nog niet want we moeten de echte file doorgegeven niet file die we zelf net hebben aangemaakt met zelfde naam
+    }
+        else {
+            file.setFileOwner(true);
+            ClientComm.replicate(file,replicationNode);
+        }
+    }
+
+    private void receiveFile(File file, Node sender) {
+
+        if (!file.isShutdown()) {
+            nodeData.addReplicated(file);
+            if (file.isFileOwner() == true)
+                file.getFileLog().put(nodeData.getThisNode(), 1);
+            nodeData.addfileLog(file);
+        } else if (file.isShutdown()) {
+            replicateFile(file.getName());
+        }
+    }
+
+
+    private void shutDown() {
+            for (Map.Entry<File, HashMap<Node,Integer>> entry : nodeData.getfileLog().entrySet()) {              //elke entry bestaat uit een file (=key) en de hashmap(=value) waarin dan weer enerzijds de nodes (=keys) te vinden zijn die deze file bezitten en een intger value (=value) die aangeeft of het om replicated, local of downloaded gaat.
+                    entry.getKey().setFileOwner(true);
+                    ClientComm.replicate(entry.getKey(), nodeData.getPrevNode());
+                    entry.getKey().getFileLog().remove(noteData);               // voor hij shutdowned moet hij zich verwijderen als bezitter van de file in de log van de file
+                    //nodeData.getfileLog().remove(entry);                      //verwijder fileLog van deze file uit lijst van fileLog want hij is niet meer owner.
+                    //gaat die for lus nog correct runnen als ik binnen de lus een entry verwijder?
+                }
+            }
+    }
+
 
 
 
---
+++ src/main/java/com/distributed/ta/FileAPI.java
@@ -0,0 +1,14 @@
+package com.distributed.ta;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+@Path("Files")
+public class FileAPI {
+
+
+    @POST
+    @Consumes()
+}
